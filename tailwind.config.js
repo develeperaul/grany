be4657ac18976322bd18f6dc095bd9f70f6ba26e
nikita <nikita45454@gmail.com>
2022-07-15 12:25:10 +0300

    feat: преднастройка tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,59 @@ module.exports = {
   content: ["./src/**/*.{vue,js}"],
   prefix: 'tw-',
   theme: {
-    extend: {},
+    screens: {
+      xl: '1920px',
+      lg: '1200px',
+      md: '768px'
+    },
+    colors: {
+      orange: '#FF6B00',
+      white: '#FFFFFF',
+      gray: '#D7D7D7',
+      primary: '#081924',
+      secondary: '#718A98',
+      dark: '#142D3E'
+    },
+    spacing: {
+      '2': '2px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '10': '10px',
+      '12': '12px',
+      '14': '14px',
+      '16': '16px',
+      '18': '18px',
+      '20': '20px',
+      '22': '22px',
+      '24': '24px',
+      '25': '25px',
+      '26': '26px',
+      '28': '28px',
+      '30': '30px',
+      '32': '32px',
+      '34': '34px',
+      '35': '35 px',
+      '40': '40px',
+      '48': '48px',
+      '60': '60px',
+      '80': '80px',
+      '120': '120px',
+    },
+    fontSize: {
+      xl20: '96px',
+      xl10: '72px',
+      xl: '48px',
+      lg: '36px',
+      md: '24px',
+      base: '16px',
+      sm: '14px',
+      xs: '12px',
+      xxs: '10px'
+    },
+    fontFamily: {
+      'nexa': ['Nexa', 'sans-serif']
+    }
   },
   plugins: [],
 }
