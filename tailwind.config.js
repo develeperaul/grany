5135fbefd34c9b470c5c393ab816b0681c2f5d7f
nikita <nikita45454@gmail.com>
2022-07-15 13:03:41 +0300

    feat: настраивает контейнер

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,11 @@ module.exports = {
   prefix: 'tw-',
   theme: {
     screens: {
-      xl: '1920px',
-      lg: '1200px',
-      md: '768px'
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1200px',
+      '2xl': '1920px',
     },
     colors: {
       orange: '#FF6B00',
