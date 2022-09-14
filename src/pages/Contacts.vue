8db7744d29102e5bccf8455f042aa688d5475f0a
nikita <nikita45454@gmail.com>
2022-09-14 20:33:07 +0500

    feat: добавляет контакты

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,14 +1,54 @@
 <template>
-  <app-page>
+  <app-page class="2xl:tw-pt-[70px]">
     <div class="wrapper">
-      <img src="/contacts.svg" alt="">
+      <h1 class="h1 tw-mb-20 2xl:tw-absolute">контакты</h1>
+      <div class="xl:tw-flex xl:tw-flex-wrap">
+        <div class="tw-mb-20 xl:tw-w-1/2 2xl:tw-w-full 2xl:tw-pl-[50%] 2xl:tw-mb-40">
+          <p class="tw-text-secondary tw-leading-120 tw-text-md tw-pb-20 md:tw-text-lg md:tw-font-extrabold md:tw-max-w-[600px]">
+            Башкортостан, г. Уфа, ул. Магистральная, 36Б
+          </p>
+          <div class="-tw-mt-10 md:tw-flex md:-tw-mx-20 xl:tw-block">
+            <div class="tw-pt-10 md:tw-px-20">
+              <a class="tw-block" href="tel:+73472586677">
+                +7 (347) 258-66-77
+              </a>
+              <a class="tw-block" href="mailto:belierosidomufa@yandex.ru">
+                belierosidomufa@yandex.ru
+              </a>
+            </div>
+            <div class="tw-pt-10 md:tw-px-20">
+              <p>Пн. - Пт. 10:00 - 17:00 (без перерыва)</p>
+              <p>Сб. - Вс. - выходные дни</p>
+            </div>
+          </div>
+        </div>
+        <div class="tw-mb-40 md:tw-mb-0 xl:tw-w-1/2 2xl:tw-order-2">
+          <yandex-map
+            class="tw-mix-blend-luminosity tw-h-[290px] md:tw-h-[390px] xl:tw-h-[580px] 2xl:tw-h-[440px]"
+            :coords="[54.709725, 55.828768]"
+            :zoom="15"
+            :controls="[]"
+          >
+            <ymap-marker
+              marker-id="place"
+              :coords="[54.709725, 55.828768]"
+              hint-content="офис - ул. Магистральная, 36Б"
+            />
+          </yandex-map>
+        </div>
+        <FormCallBack class="cover -tw-mx-16 md:tw-mx-0 xl:tw-w-1/2 xl:tw-ml-auto 2xl:tw-order-1"/>
+      </div>
     </div>
   </app-page>
 </template>
 
 <script>
-export default {
+import FormCallBack from '@/components/FormCallBack.vue';
 
+export default {
+  components: {
+    FormCallBack
+  }
 }
 </script>
 
--- src/registr-plugins.js
+++ src/registr-plugins.js
@@ -1,5 +1,15 @@
-import vClickOutside from "click-outside-vue3"
+import vClickOutside from "click-outside-vue3";
+import YmapPlugin from 'vue-yandex-maps';
 
 export default (app) => {
+  const settings = {
+    apiKey: 'ff37a483-416c-4661-8877-cc44eb1abf7b',
+    lang: 'ru_RU',
+    coordorder: 'latlong',
+    enterprise: false,
+    version: '2.1'
+  }
+
   app.use(vClickOutside);
+  app.use(YmapPlugin, settings)
 }
