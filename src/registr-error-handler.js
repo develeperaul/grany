1f03fd2a2a5ad886e9146f90fcd274f76644125a
nikita <nikita45454@gmail.com>
2022-10-10 11:48:57 +0500

    fix: изменяет коды ошибок в глобальном обработчике

--- src/registr-error-handler.js
+++ src/registr-error-handler.js
@@ -4,7 +4,13 @@ export default (app) => {
   app.config.errorHandler = function (err, vm, info) {
     if(err instanceof HTTPError) {
 
-      if (err.response.status === 500) return vm.$notify({
+      if (err.response.status === 429) return vm.$notify({
+        group: 'error',
+        type: 'error',
+        text: 'Вы превысили максимальное количество попыток'
+      });
+
+      return vm.$notify({
         group: 'error',
         type: 'error',
         text: 'Произошла ошибка на сервере'
