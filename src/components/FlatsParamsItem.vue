821d6a0c9896260db4499900b161e7a66eeb6154
nikita <nikita45454@gmail.com>
2022-10-10 22:36:21 +0500

    feat: связывает с сервером список фильра квартир

--- src/components/FlatsParamsItem.vue
+++ src/components/FlatsParamsItem.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="tw-text-secondary tw-py-20 tw-px-16 tw-border-b md:tw-border-r tw-border-gray md:tw-w-1/2 tw-flex tw-flex-wrap">
+  <router-link
+    class="tw-text-secondary tw-py-20 tw-px-16 tw-border-b md:tw-border-r tw-border-gray md:tw-w-1/2 tw-flex tw-flex-wrap xl:hover:tw-bg-[#f1f1f1] xl:tw-transition-colors"
+    :to="{ name: 'flats.one', params: { id: item.id } }"
+  >
     <div class="xl:tw-w-1/2 2xl:tw-w-1/3">
       <p class="tw-text-dark tw-text-lg tw-font-extrabold tw-leading-100 tw-mb-10">
-        99.5 м2
+        {{ item.total_area }} <span>м<sup>2</sup></span>
       </p>
       <div class="tw-mb-6">
-        <p>Литер 1  •  Этаж 7  •  3 комнаты</p>
+        <p>Литер {{ item.house_name }}  •  Этаж {{item.storey_number}} •  {{ item.rooms_number }} комнаты</p>
       </div>
     </div>
 
-    <div class="tw-mb-20 xl:tw-w-1/2 2xl:tw-w-1/3">
-      <img class="xl:tw-w-[160px]" src="/flat.png" alt="квартира 99.5 м2">
+    <div class="tw-mb-20 tw-w-full xl:tw-w-1/2 2xl:tw-w-1/3">
+      <img width="686" height="428" class="tw-w-full xl:tw-w-[160px]" src="/flat.png" alt="квартира 99.5 м2">
     </div>
 
     <div class="tw-w-full tw-self-start tw-flex tw-flex-wrap -tw-mt-10 -tw-mx-8 tw-max-w-[450px] 2xl:tw-w-1/3">
@@ -19,11 +22,21 @@
       <div class="xl:tw-w-1/3 tw-w-1/2 2xl:tw-w-1/2 tw-px-8 tw-pt-10">Кухня-гостиная</div>
       <div class="xl:tw-w-1/3 tw-w-1/2 2xl:tw-w-1/2 tw-px-8 tw-pt-10">Видовая квартира</div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script>
 export default {
-
+  props: {
+    item: {
+      required: true,
+      type: Object
+    }
+  }
 }
 </script>
+<style scoped>
+.item:hover > div:first-child {
+  opacity: 0.7;
+}
+</style>
--- src/components/FlatsParamsList.vue
+++ src/components/FlatsParamsList.vue
@@ -1,9 +1,14 @@
 <template>
   <div
     class="-tw-mx-16 md:tw-mx-0 tw-flex tw-flex-wrap tw-border-gray md:tw-border-l"
-    :class="{ 'tw-border-t': len > 1 }"
+    :class="{ 'tw-border-t': items.length > 1 }"
   >
-    <FlatsParamsItem v-for="i in len" :key="i" :class="{ 'tw-border-t': len === 1 }" />
+    <FlatsParamsItem
+      :class="{ 'tw-border-t': items.length === 1 }"
+      v-for="item in items"
+      :key="item.id"
+      :item="item"
+    />
   </div>
 </template>
 
@@ -11,6 +16,12 @@
 import FlatsParamsItem from '@/components/FlatsParamsItem.vue';
 
 export default {
+  props: {
+    items: {
+      required: true,
+      type: Array
+    }
+  },
   data() {
     return {
       len: 5
